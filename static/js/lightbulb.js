c3ed854759c782e431325c0af969355a95f5d5c5
Nick Tzaperas <nick@nijotz.com>
2015-06-24 17:08:15 -0700

    A cube with some light and some movement

--- static/js/lightbulb.js
+++ static/js/lightbulb.js
@@ -1,3 +1,4 @@
+// Setup
 var renderer = null;
 var canvas = document.getElementById("lightbulb-canvas");
 canvas.width  = canvas.offsetWidth;
@@ -14,29 +15,87 @@ function webglAvailable() {
     }
 }
 
+// Renderer
 if ( webglAvailable() ) {
     renderer = new THREE.WebGLRenderer({"canvas": canvas});
 } else {
     renderer = new THREE.CanvasRenderer({"canvas": canvas});
 }
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 1000 );
 renderer.setSize( canvas.width, canvas.height );
-renderer.setClearColor(0x696060);
+renderer.setClearColor( 0 );
+renderer.shadowMapEnabled = true;
+renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
-//var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-//var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-//var cube = new THREE.Mesh( geometry, material );
-//scene.add( cube );
+// Camera
+camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 100000 );
+camera.position.x = 1200;
+camera.position.y = 1000;
+camera.lookAt({
+  x: 0,
+  y: 0,
+  z: 0
+});
 
-camera.position.z = 5;
+// Scene
+var scene = new THREE.Scene();
 
+// Cube & Ground
+var geometry = new THREE.BoxGeometry( 100, 100, 100 );
+var material = new THREE.MeshLambertMaterial( { color: 0x0aeedf } );
+var cube = new THREE.Mesh( geometry, material );
+cube.castShadow = true;
+cube.position.x = 0;
+cube.position.y = 100;
+cube.position.z = 0;
+scene.add( cube );
+
+var geometry = new THREE.PlaneGeometry( 500, 500 );
+var material = new THREE.MeshPhongMaterial({ color: 0x6C6C6C });
+var ground = new THREE.Mesh( geometry, material );
+ground.rotation.x = -Math.PI / 2;
+ground.receiveShadow = true;
+scene.add( ground );
+
+// Lights
+scene.add(new THREE.AmbientLight(0));
+
+var light = new THREE.DirectionalLight( 0xdfebff, 1.75 );
+light.position.set( 300, 400, 50 );
+//light.multiplyScalar(1.3);
+light.castShadow = true;
+//light.shadowCameraVisible = true;
+light.shadowMapWidth = 512;
+light.shadowMapHeight = 512;
+
+var d = 200;
+
+light.shadowCameraLeft = -d;
+light.shadowCameraRight = d;
+light.shadowCameraTop = d;
+light.shadowCameraBottom = -d;
+
+light.shadowCameraFar = 1000;
+light.shadowDarkness = 0.2;
+
+scene.add( light );
+
+// Update & Render
 function render() {
+    update();
     requestAnimationFrame( render );
     renderer.render( scene, camera );
 }
 render();
+
+function update() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.02;
+
+  var timer = Date.now() * 0.0002;
+  camera.position.x = Math.cos(timer) * 1000;
+  camera.position.z = Math.sin(timer) * 1000;
+  camera.lookAt(scene.position);
+}
 
 window.addEventListener( 'resize', onWindowResize, false );
 
@@ -49,5 +108,4 @@ function onWindowResize(){
     camera.updateProjectionMatrix();
 
     renderer.setSize( canvas.width, canvas.height );
-
 }
