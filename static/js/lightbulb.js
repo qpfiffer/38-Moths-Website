bbd7aec4efb2d6ff6d329c0a8aae05c972fb0201
Nick Tzaperas <nick@nijotz.com>
2015-06-25 17:57:05 -0700

    Remove cruft

--- static/js/lightbulb.js
+++ static/js/lightbulb.js
@@ -25,8 +25,6 @@ if ( webglAvailable() ) {
 }
 renderer.setSize( canvas.width, canvas.height );
 renderer.setClearColor( 0 );
-renderer.shadowMapEnabled = true;
-renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 // Camera
 camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 100000 );
@@ -41,44 +39,10 @@ camera.lookAt({
 // Scene
 var scene = new THREE.Scene();
 
-// Cube & Ground
-var geometry = new THREE.BoxGeometry( 100, 100, 100 );
-var material = new THREE.MeshLambertMaterial( { color: 0x0aeedf } );
-var cube = new THREE.Mesh( geometry, material );
-cube.castShadow = true;
-cube.position.x = 0;
-cube.position.y = 100;
-cube.position.z = 0;
-//scene.add( cube );
-
-var geometry = new THREE.PlaneGeometry( 500, 500 );
-var material = new THREE.MeshPhongMaterial({ color: 0x6C6C6C });
-var ground = new THREE.Mesh( geometry, material );
-ground.rotation.x = -Math.PI / 2;
-ground.receiveShadow = true;
-//scene.add( ground );
-
-// Lights
-scene.add(new THREE.AmbientLight(0));
-
-var light = new THREE.DirectionalLight( 0xdfebff, 1.75 );
-light.position.set( 300, 400, 50 );
-//light.multiplyScalar(1.3);
+// Lightbulb
+var light = new THREE.PointLight( 0xdfebff, 1.75 );
+light.position.set( 0, 0, 0 );
 light.castShadow = true;
-//light.shadowCameraVisible = true;
-light.shadowMapWidth = 512;
-light.shadowMapHeight = 512;
-
-var d = 200;
-
-light.shadowCameraLeft = -d;
-light.shadowCameraRight = d;
-light.shadowCameraTop = d;
-light.shadowCameraBottom = -d;
-
-light.shadowCameraFar = 1000;
-light.shadowDarkness = 0.2;
-
 scene.add( light );
 
 // Moths
@@ -86,9 +50,7 @@ var Moth = function() {
     var geometry = new THREE.BoxGeometry( 10, 10, 10 );
     var material = new THREE.MeshLambertMaterial( { color: 0x0aeedf } );
     this.object = new THREE.Mesh( geometry, material );
-    this.object.x = 0;
-    this.object.y = 100;
-    this.object.z = 0;
+    this.object.position = new THREE.Vector3(0, 100, 0);
     this.velocity = new THREE.Vector3(1, 1, 1);
     scene.add(this.object);
 }
@@ -110,8 +72,6 @@ function render() {
 render();
 
 function update() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.02;
 
     var timer = Date.now() * 0.0002;
     camera.position.x = Math.cos(timer) * 1000;
